93dcc322aa23f9c6876c1b1d93630d8664fb8733
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-06-25 01:24:05 -0300

    add function to hit api & create location class

---
+++ src/components/logic.js
@@ -0,0 +1,32 @@
+export {getLocationData}
+
+async function getLocationData() {
+    const search = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=2JD7QNU3CZCB4MPLH6EXHAUFV", {mode: "cors"});
+    const locationData = await search.json();
+    const temperature = locationData.currentConditions.temp;
+    const description = locationData.description;
+    const condition = locationData.currentConditions.conditions;
+    const iconData = locationData.currentConditions.icon;
+    const name = locationData.resolvedAddress;
+    const time = locationData.currentConditions.datetime;
+    const upcomingDays = locationData.days;
+    const currentLocation = new Location(temperature, description, condition, iconData, name, time, upcomingDays);
+    console.log(currentLocation)
+    console.log(locationData);
+    console.log(upcomingDays);
+    console.log(temperature);
+    console.log(description, condition, iconData, name, time);
+}
+
+class Location{
+    constructor(temperatureF, description, condition, iconData, name, time, upcomingDays) {
+        this.temperatureF = temperatureF;
+        this.temperatureC = (temperatureF - 32) / 1.8;
+        this.description = description;
+        this.condition = condition;
+        this.iconData = iconData;
+        this.name = name;
+        this.time = time;
+        this.upcomingDays = upcomingDays;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles/main.css";
+import { getLocationData } from "./components/logic";
+
+getLocationData();
