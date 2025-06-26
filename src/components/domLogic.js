047d7d8d2537f6799f48e355a4b502f40fabed93
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-06-26 01:55:12 -0300

    add assets, create structure for main

--- src/components/domLogic.js
+++ src/components/domLogic.js
@@ -1,13 +1,13 @@
 import { getLocationData } from "./logic";
-export { searchLocation }
+export { searchLocation };
 
 function searchLocation() {
-    const button = document.getElementById("search-submit");
-    const userQuery = document.getElementById("city-search");
+  const button = document.getElementById("search-submit");
+  const userQuery = document.getElementById("city-search");
 
-    button.addEventListener("click", (event) => {
-        event.preventDefault()
-        //console.log("hello");
-        getLocationData(userQuery.value.trim().toLowerCase());
-    })
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    //console.log("hello");
+    getLocationData(userQuery.value.trim().toLowerCase());
+  });
 }
