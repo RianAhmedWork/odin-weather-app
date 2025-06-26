6f334d4dae93d19fddd850718dccac8bda8b19cd
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-06-26 15:51:53 -0300

    add try catch to async function

--- src/components/logic.js
+++ src/components/logic.js
@@ -1,8 +1,12 @@
-export {getLocationData}
+export { getLocationData };
 
 async function getLocationData(query) {
-    const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + query + "?key=2JD7QNU3CZCB4MPLH6EXHAUFV";
-    const search = await fetch(url, {mode: "cors"});
+  try {
+    const url =
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+      query +
+      "?key=2JD7QNU3CZCB4MPLH6EXHAUFV";
+    const search = await fetch(url, { mode: "cors" });
     const locationData = await search.json();
     const temperature = locationData.currentConditions.temp;
     const description = locationData.description;
@@ -11,23 +15,42 @@ async function getLocationData(query) {
     const name = locationData.resolvedAddress;
     const time = locationData.currentConditions.datetime;
     const upcomingDays = locationData.days;
-    const currentLocation = new Location(temperature, description, condition, iconData, name, time, upcomingDays);
-    console.log(currentLocation)
+    const currentLocation = new Location(
+      temperature,
+      description,
+      condition,
+      iconData,
+      name,
+      time,
+      upcomingDays
+    );
+    console.log(currentLocation);
     console.log(locationData);
     console.log(upcomingDays);
     console.log(temperature);
     console.log(description, condition, iconData, name, time);
+  } catch (error) {
+    console.log(`There has been an error ${error}`);
+  }
 }
 
-class Location{
-    constructor(temperatureF, description, condition, iconData, name, time, upcomingDays) {
-        this.temperatureF = temperatureF.toFixed(1);
-        this.temperatureC = ((temperatureF - 32) / 1.8).toFixed(1);
-        this.description = description;
-        this.condition = condition;
-        this.iconData = iconData;
-        this.name = name;
-        this.time = time;
-        this.upcomingDays = upcomingDays;
-    }
+class Location {
+  constructor(
+    temperatureF,
+    description,
+    condition,
+    iconData,
+    name,
+    time,
+    upcomingDays
+  ) {
+    this.temperatureF = temperatureF.toFixed(1);
+    this.temperatureC = ((temperatureF - 32) / 1.8).toFixed(1);
+    this.description = description;
+    this.condition = condition;
+    this.iconData = iconData;
+    this.name = name;
+    this.time = time;
+    this.upcomingDays = upcomingDays;
+  }
 }
